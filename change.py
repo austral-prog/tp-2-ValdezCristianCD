4c267dca701dcc8c83ba0c9d4d9eed949e1c4691
ValdezCristianCD <cvaldez2@mail.austral.edu.ar>
2025-03-31 11:21:53 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,3 +1,15 @@
 def change():
     expense = 23.75
     money = 100
+    
+    print(f"Ingresar gasto:")
+    print(f"{expense}")
+    print(f"Dinero recibido")
+    print(f"{money}")
+
+    print(f"Vuelto")
+
+    print(f"Pesos:")
+    print(f"{str(money - expense).split(".")[0]}")
+    print(f"Centavos:")
+    print(f"{str(money - expense).split(".")[1]}")
