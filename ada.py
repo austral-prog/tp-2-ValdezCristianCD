45bf8ab6550123d5765813195b9d9cef8a730a86
ValdezCristianCD <cvaldez2@mail.austral.edu.ar>
2025-03-31 11:21:00 -0300

    Update ada.py

--- ada.py
+++ ada.py
@@ -1,3 +1,8 @@
 def ada():
     first_name = "AdA"
     last_name = "LoVeLAce"
+
+    print(f"{first_name.lower()} {last_name.lower()}")
+    print(f"{first_name.capitalize()} {last_name.capitalize()}")
+    print(f"{first_name.upper()} {last_name.upper()}")
+    print(f"	{first_name.lower()} {last_name.lower()}")
